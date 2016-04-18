8948dc9a6454a2b48cbb02407fee2769b7eb366d
dam110 <dam110@PC2010.aulas.local>
2016-04-18 10:43:49 +0200

    Primera prueba de conexión entre dos personas

---
+++ PracticaGit-master/src/practicagit/Figura.java
@@ -0,0 +1,17 @@
+
+package practicagit;
+
+abstract class Figura {
+    String color;
+    
+    public Figura(String color){
+        this.color=color;
+    }
+    
+    public float area(){
+        return 0;
+    }
+     public float perimetro(){
+        return 0;
+    }
+}
